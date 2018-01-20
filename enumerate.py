3979136ab635787075f93107e7d9dd459d143054
Mohanish Gunwant <mohanish.gunwant@dal.ca>
2018-01-20 09:09:30 -0800

    commit

---
+++ enumerate.py
@@ -0,0 +1,12 @@
+Products=['veggies','kurkure','soap','milk','juice','spices','cookies']
+for i in  enumerate(Products):
+    print(i)
+    print("\n")
+for count,i in enumerate(Products):
+    print(count,i)
+  
+    print("\n")
+for count,i in enumerate(Products,10):
+    print(count,i)
+
+

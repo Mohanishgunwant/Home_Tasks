4db9c791a28abe223639bf2f05da21f1d7a46d67
Mohanish Gunwant <mohanish.gunwant@dal.ca>
2018-01-20 11:41:03 -0800

    commit

---
+++ pos_neg_zero.py
@@ -0,0 +1,10 @@
+def pos_neg_zero():
+    x=float(input("Enter a number\n"))
+    if x==0:
+        print("{0} is Zero".format(x))
+    elif x>0:
+        print("{0} is positive".format(x))
+    else:
+        print("{0} is negative".format(x))
+pos_neg_zero()
+

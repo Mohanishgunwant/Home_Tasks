6d2b2828d2cef09d6c348b1f42ab4378818fc3f3
Mohanish Gunwant <mohanish.gunwant@dal.ca>
2018-01-21 12:01:23 -0800

    commit

---
+++ swap_2_variable.py
@@ -0,0 +1,8 @@
+#program to swap value of two variables
+a=(input("Enter variable one\n"))
+b=(input("Enter variable two\n"))
+s=a
+a=b
+b=s
+#print("variable a is now ",a,"and variable b is now ",b)
+print("variable a is now changed to {0} and variable b is now changed to {1}".format(a,b))
